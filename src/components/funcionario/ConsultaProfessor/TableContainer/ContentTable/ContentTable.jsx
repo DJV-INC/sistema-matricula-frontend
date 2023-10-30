06ff8e195656d0287a96808db0c2972caa7454e7
Carjooj <victor@4info.com.br>
2023-10-29 21:08:19 -0300

    Professor - ContentTable
    ContentTable do professor para ficar com uma funcionalidade similiar ao consulta turma

--- src/components/funcionario/ConsultaProfessor/TableContainer/ContentTable/ContentTable.jsx
+++ src/components/funcionario/ConsultaProfessor/TableContainer/ContentTable/ContentTable.jsx
@@ -1,81 +1,58 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Table } from "reactstrap";
+//import Dropdown from "./Dropdown/Dropdown";
 
 import './ContentTable.css'
 
-export default function ContentTable({ contentData }) {
-  const [dropdown, setDropdown] = useState(false)
+export default function ContentTable({ contentData, setId}) {
+
   const [data, setData] = useState({ dados: [] })
 
   useEffect(() => {
     setData(contentData)
   }, [contentData]);
 
-  function dropdownToggle(event) {
-    const nodelistDropdowns = document.querySelectorAll(".dropdown-container")
-    const btn = event.currentTarget
+  function handleRowTable(id, event) {
+    const container = event.currentTarget.parentNode
+    const trList = container.getElementsByTagName("tr")
 
-    for (let i = 0; i < nodelistDropdowns.length; i++) {
-      setDropdown(false)
-      nodelistDropdowns[i].classList.remove("active")
+    for (let index = 0; index < trList.length; index++) {
+      trList[index].classList.remove("active")
     }
 
-    document.body.addEventListener("click", (e) => {
-      console.log(e.target);
-      if (e.target !== btn || e.target !== btn.querySelector(".dropdown-container") || e.target !== event.target) {
-        if (dropdown) {
-          setDropdown(false)
-        } else {
-          setDropdown(true)
-        }
-      }
-    })
+    setId(id)
 
-    if (!dropdown) {
-      btn.querySelector(".dropdown-container").classList.add("active")
-    } else {
-      btn.querySelector(".dropdown-container").classList.remove("active")
-      for (let i = 0; i < nodelistDropdowns.length; i++) {
-        nodelistDropdowns[i].classList.remove("active")
-      }
-    }
+    event.currentTarget.classList.add("active")
   }
 
   try {
 
     return (
       <Fragment>
-
-        <table className="table_">
-          <thead>
-            <tr className="tr_">
-              <th>Nome</th>
-              <th>CPF</th>
-              {/* <th>Status</th>
-              <th>Opções</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            {
-              data.dados.map((item) => {
-                return(
+        <div className="Tabelas">
+          <div className="TabProfessor">
+            <Table striped>
+              <thead>
                 <tr>
-                  <td>{item.nomeCompleto}</td>
-                  <td>{item.cpf}</td>
-                  {/* <td><Status status={item.statusMatricula} /></td>
-                  <td>
-                    <button className="options-btn" onClick={dropdownToggle}>
-                      <span className="material-symbols-rounded">more_vert</span>
-                      <Dropdown id={item.cpf} />
-                    </button>
-                  </td> */}
+                  <th>Nome</th>
+                  <th>CPF</th>
                 </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-
+              </thead>
+              <tbody className="item-professor-table">
+                {
+                  data.dados.map((item) => {
+                    return (
+                      <tr onClick={(e) => {handleRowTable(item.id, e)}}>
+                        <td>{item.nomeCompleto}</td>
+                        <td>{item.cpf}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </Table>
+          </div>
+        </div>
       </Fragment>
     );
   } catch (error) {
@@ -83,3 +60,95 @@ export default function ContentTable({ contentData }) {
   }
 
 }
+
+
+
+
+
+
+
+// import React, { Fragment, useEffect, useState } from "react";
+// import { Table } from "reactstrap";
+
+// import './ContentTable.css'
+
+// export default function ContentTable({ contentData }) {
+//   const [dropdown, setDropdown] = useState(false)
+//   const [data, setData] = useState({ dados: [] })
+
+//   useEffect(() => {
+//     setData(contentData)
+//   }, [contentData]);
+
+//   function dropdownToggle(event) {
+//     const nodelistDropdowns = document.querySelectorAll(".dropdown-container")
+//     const btn = event.currentTarget
+
+//     for (let i = 0; i < nodelistDropdowns.length; i++) {
+//       setDropdown(false)
+//       nodelistDropdowns[i].classList.remove("active")
+//     }
+
+//     document.body.addEventListener("click", (e) => {
+//       console.log(e.target);
+//       if (e.target !== btn || e.target !== btn.querySelector(".dropdown-container") || e.target !== event.target) {
+//         if (dropdown) {
+//           setDropdown(false)
+//         } else {
+//           setDropdown(true)
+//         }
+//       }
+//     })
+
+//     if (!dropdown) {
+//       btn.querySelector(".dropdown-container").classList.add("active")
+//     } else {
+//       btn.querySelector(".dropdown-container").classList.remove("active")
+//       for (let i = 0; i < nodelistDropdowns.length; i++) {
+//         nodelistDropdowns[i].classList.remove("active")
+//       }
+//     }
+//   }
+
+//   try {
+
+//     return (
+//       <Fragment>
+
+//         <table className="table_">
+//           <thead>
+//             <tr className="tr_">
+//               <th>Nome</th>
+//               <th>CPF</th>
+//               {/* <th>Status</th>
+//               <th>Opções</th> */}
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {
+//               data.dados.map((item) => {
+//                 return(
+//                 <tr>
+//                   <td>{item.nomeCompleto}</td>
+//                   <td>{item.cpf}</td>
+//                   {/* <td><Status status={item.statusMatricula} /></td>
+//                   <td>
+//                     <button className="options-btn" onClick={dropdownToggle}>
+//                       <span className="material-symbols-rounded">more_vert</span>
+//                       <Dropdown id={item.cpf} />
+//                     </button>
+//                   </td> */}
+//                 </tr>
+//                 )
+//               })
+//             }
+//           </tbody>
+//         </table>
+
+//       </Fragment>
+//     );
+//   } catch (error) {
+
+//   }
+
+// }
