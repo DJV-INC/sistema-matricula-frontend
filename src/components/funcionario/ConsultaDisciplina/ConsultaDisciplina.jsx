516c248e34ad0e065628100ad186e27511ae6e82
Carjooj <victor@4info.com.br>
2023-10-31 00:17:15 -0300

    Componente ConsultaDisciplina
    Removendo CSS obsoleto, adicionando className ao titulo e subtitulo

--- src/components/funcionario/ConsultaDisciplina/ConsultaDisciplina.jsx
+++ src/components/funcionario/ConsultaDisciplina/ConsultaDisciplina.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-import './ConsultaDisciplina.css'
 import TableContainer from './TableContainer/TableContainer'
 
 export default function ConsultaDisciplina() {
   return (
       <div className="container-main">
         <div className='page-title'>
-          <h1>Disciplinas</h1>
-          <span>Registros</span>
+        <h1 className='title_'>Disciplinas</h1>
+        <p className='subtitle_'>Registros</p>
         </div>
         <TableContainer/>
       </div>
