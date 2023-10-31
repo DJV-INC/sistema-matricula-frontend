29c0bcc600448a0324ee50e3767397ff6b110963
Carjooj <victor@4info.com.br>
2023-10-31 00:16:32 -0300

    Componente ConsultaAluno
    Removendo css obsoleto, adicionando className no titulo e subtitulo

--- src/components/funcionario/ConsultaAluno/ConsultaAluno.jsx
+++ src/components/funcionario/ConsultaAluno/ConsultaAluno.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-import './ConsultaAluno.css'
 import TableContainer from './TableContainer/TableContainer'
 
 export default function ConsultaAluno() {
   return (
       <div className="container-main">
         <div className='page-title'>
-          <h1>Alunos</h1>
-          <span>Registros</span>
+          <h1 className='title_'>Alunos</h1>
+          <p className='subtitle_'>Registros</p>
         </div>
         <TableContainer/>
       </div>
