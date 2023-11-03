c49a3e42aa278d4de2eab8c32c5fe5acf87632fb
douglasdans <douglas.dans@gmail.com>
2023-11-03 19:52:10 -0300

    Routes | Refatorado arquivo

--- src/router.jsx
+++ src/router.jsx
@@ -4,8 +4,6 @@ import React from "react";
 import FuncionarioAluno from "./pages/FuncionarioAluno";
 import FuncionarioDisciplina from "./pages/FuncionarioDisciplina";
 import FuncionarioProfessor from "./pages/FuncionarioProfessor";
-import DeleteTurma from "./components/funcionario/ConsultaTurma/Modal/DeleteTurma/DeleteTurma";
-import EditTurma from "./components/funcionario/ConsultaTurma/Modal/EditTurma/EditTurma";
 
 export default function Router() {
   	return (
@@ -17,9 +15,11 @@ export default function Router() {
 			<Route exact path="/disciplina" element={<FuncionarioDisciplina/>}/>
 				<Route exact path="/disciplina/:modalType/:id" element={<FuncionarioDisciplina/>}/>
 				<Route exact path="/disciplina/:modalType" element={<FuncionarioDisciplina/>}/>	
+
 					<Route exact path="/disciplina/:idDisciplina/turma/:modalType/:idTurma" element={<FuncionarioDisciplina/>}/>
 					<Route exact path="/disciplina/:idDisciplina/turma/:modalType" element={<FuncionarioDisciplina/>}/>
 					<Route exact path="/disciplina/turma/:modalType" element={<FuncionarioDisciplina/>}/>
+
 			<Route exact path="/professor" element={<FuncionarioProfessor/>}/>
 			<Route exact path="/professor/:modalType/:id" element={<FuncionarioProfessor/>}/>
 			<Route exact path="/professor/:modalType" element={<FuncionarioProfessor/>}/>
