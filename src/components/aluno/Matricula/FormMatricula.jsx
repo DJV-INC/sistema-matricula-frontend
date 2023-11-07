98b077063dcec816b9eee9cadc2ece7b6e32d0b2
Carjooj <victor@4info.com.br>
2023-11-07 20:34:24 -0300

    FormMatricula.jsx
    Step do form funcionando com os botões (componentes) customizados
    
    Co-authored-by: Jackeline Paula <jackelinepaula@users.noreply.github.com>

--- src/components/aluno/Matricula/FormMatricula.jsx
+++ src/components/aluno/Matricula/FormMatricula.jsx
@@ -3,30 +3,37 @@ import { useState, useEffect } from 'react'
 import DisObrigatoria from './StepForms/DisObrigatoria'
 import DisOptativa from './StepForms/DisOptativa'
 import FinalForm from './StepForms/FinalForm'
+import StepButton from './StepButton/StepButton'
 
+import './Matricula.css'
 
 export default function FormMatricula() {
-    // const [step, setStep] = useState("")
-    // const [comp, setComp] = useState("")
+    const [step, setStep] = useState("")
+    const [comp, setComp] = useState("")
 
-    // const steps = [1, 2, 3]
+    const steps = [1, 2, 3]
 
-    // function getCurrStep(){
-    //     switch (step) {
-    //         case 1:
-    //             return <DisObrigatoria />
+    function getCurrStep() {
+        switch (step) {
+            case "1":
+                return <DisObrigatoria/>
 
-    //         case 2:
-    //             return <DisOptativa />
+            case "2":
+                return <DisOptativa/>
 
-    //         case 3:
-    //             return <FinalForm />
-    //     }
-    // }
+            case "3":
+                return <FinalForm/>
 
-    // useEffect(()=>{
-    //      setComp(getCurrStep())
-    // },[step]) 
+        }
+    }
+
+    function handleStep(e) {
+        setStep(e.target.value)
+    }
+
+    useEffect(() => {
+        setComp(getCurrStep())
+    }, [step])
 
     return (
         <div className="container-main">
@@ -35,13 +42,24 @@ export default function FormMatricula() {
                 <p className='subtitle_'>Registrar</p>
             </div>
 
-            {/* <button onClick={setStep(1)} style={{width: '120px'}}>1</button>
-            <button onClick={setStep(2)} style={{width: '120px'}}>2</button>
-            <button onClick={setStep(3)} style={{width: '120px'}}>3</button>
-            
-            <div className="form">
-                {comp ? comp :  <DisObrigatoria/>}
-            </div> */}
+            <div className="content-matricula">
+
+                <div className="container-botao">
+                    <StepButton funcao={handleStep} texto={"Disciplina obrigatórias"} index={1} step={step} />
+
+                    <span className="material-symbols-rounded flecha">chevron_right</span>
+
+                    <StepButton funcao={handleStep} texto={"Disciplina optativas"} index={2} step={step} />
+
+                    <span className="material-symbols-rounded flecha">chevron_right</span>
+
+                    <StepButton funcao={handleStep} texto={"Finalização"} index={3} step={step} />
+                </div>
+
+                {comp ? comp : <DisObrigatoria/> }
+
+
+            </div>
         </div>
     )
 }
