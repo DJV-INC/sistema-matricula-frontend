8d98c84aef7832d434d72befd2699882b8001a1d
Carjooj <victor@4info.com.br>
2023-11-06 23:51:42 -0300

    ConsultaMatricula.jsx
    Criado componente para consultar a matricula

---
+++ src/components/aluno/Matricula/ConsultaMatricula.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ConsultaMatricula() {
+   return (
+      <div className="container-main">
+         <div className='page-title'>
+            <h1 className='title_'>Matrícula</h1>
+            <p className='subtitle_'>Registro</p>
+         </div>
+      </div>
+   )
+}
