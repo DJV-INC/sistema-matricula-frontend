fb5385fbe55ecc219db3a7d436d9d14593e1db8c
Carjooj <victor@4info.com.br>
2023-11-07 20:37:02 -0300

    DisObrigatoria.jsx
    Iniciado componente do formulário
    de matrícula das disciplinas obrigatórias
    
    Co-authored-by: Jackeline Paula <jackelinepaula@users.noreply.github.com>

--- src/components/aluno/Matricula/StepForms/DisObrigatoria.jsx
+++ src/components/aluno/Matricula/StepForms/DisObrigatoria.jsx
@@ -1,9 +1,25 @@
-import {React} from 'react'
+import { React } from 'react'
+import { useState, useEffect } from 'react'
 
-export default function DisObrigatoria(){
-    return(
-        <>
-        <h2>Disciplinas Obrigatórias</h2>
-        </>
+export default function DisObrigatoria() {
+    const [formDis, setFormDis] = useState("")
+
+
+    const disciplinas = ["Harmonia", "Contraponto", "Teoria Musical"]
+
+    return (
+        <div className="form">
+
+            <div className="menu-disciplina">
+                {disciplinas.map((disciplina) => {
+                    return (
+                        <button className="btn-dis">{disciplina}</button>
+                    )
+                })}
+            </div>
+            <div className="form-disciplina">
+                <p>Selecione uma matéria para iniciar a matrícula</p>
+            </div>
+        </div>
     )
 }
