b09368bd7f91f23f6def39f669bb8fa2801dd82a
douglasdans <douglas.dans@gmail.com>
2023-11-08 20:47:23 -0300

    Login | Adicionado tela provisoria

--- src/components/NavbarFuncionario.jsx
+++ src/components/NavbarFuncionario.jsx
@@ -20,9 +20,9 @@ export default function NavbarFuncionario() {
          </NavbarBrand>
 
          <div className="navlinks-container">
-            <NavLink to={"/"} className={"navlink"}>Alunos</NavLink>
-            <NavLink to={"/disciplina"} className={"navlink"}>Disciplinas</NavLink>
-            <NavLink to={"/professor"} className={"navlink"}>Professores</NavLink>
+            <NavLink to={"/admin"} className={"navlink"}>Alunos</NavLink>
+            <NavLink to={"/admin/disciplina"} className={"navlink"}>Disciplinas</NavLink>
+            <NavLink to={"/admin/professor"} className={"navlink"}>Professores</NavLink>
          </div>
       </nav>
     </header>
