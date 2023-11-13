fcbb09102bb1a7998114fa089c5fed4e5d8f2007
douglasdans <douglas.dans@gmail.com>
2023-11-13 18:35:13 -0300

    Login | Tela repaginada

--- src/components/login/FormLogin.jsx
+++ src/components/login/FormLogin.jsx
@@ -4,17 +4,34 @@ import { Link } from 'react-router-dom'
 export default function FormLogin() {
    return (
       <div className="loginContainer">
-         <form className='form-login'>
+         <form className='container-login'>
             <img src="/emm-logo2.png" alt="" />
 
-            <h1>Login</h1>
+            <h4>Login</h4>
 
-            <Link to={"/aluno/1/matricula"} >
-               <button className='login-btn'>Logar como aluno</button>
-            </Link>
-            <Link to={"/"}>
-               <button className='login-btn'>Logar como funcionário</button>
-            </Link>
+            <form className="form-login" action="" method="post">
+               <input type="text" placeholder='CPF' />
+               <input type="password" placeholder='Senha' />
+
+               <div className="login-links">
+                  <Link to={"/"}>
+                     Logar como funcionário
+                  </Link>
+
+                  <div>
+                     <Link to={"/aluno/1/matricula"} >
+                        Esqueci minha senha
+                     </Link>
+                     <Link to={"/aluno/1/matricula"} >
+                        Não tenho login
+                     </Link>
+                  </div>
+
+               </div>
+               <Link to={"/aluno/1/matricula"} className='login-btn' >
+                  Fazer Login
+               </Link>
+            </form>
          </form>
       </div>
    )
