9f53af06fd09db428fdd16cbdce81366b4f73f5a
Carjooj <victor@4info.com.br>
2023-11-21 11:23:21 -0300

    ConsultaMatricula.jsx
    Modal de exclusão adicionado e funcionando
    
    Botão para desfazer matricula
    
    Co-authored-by: Jackeline Paula <jackelinepaula@users.noreply.github.com>

--- src/components/aluno/Matricula/ConsultaMatricula.jsx
+++ src/components/aluno/Matricula/ConsultaMatricula.jsx
@@ -2,10 +2,33 @@ import { React, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import API from '../../../services/API'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-export default function ConsultaMatricula() {
+export default function ConsultaMatricula(args) {
    const [matriculas, setMatriculas] = useState([])
    const { idAluno } = useParams()
+
+   const [modal, setModal] = useState(false)
+   const [idMatricula, setIdMatricula] = useState("")
+
+   function openModal(e) {
+      toggle()
+      getIdMatricula(e)
+   }
+   
+   function toggle() {
+      setModal(!modal)
+   }
+
+   function desfazerMatricula() {
+      API.del("matriculas", idMatricula)
+      toggle()
+   }
+   
+   function getIdMatricula(e){
+      console.log(e.target.value)
+      setIdMatricula(e.target.value)
+   }
 
    useEffect(() => {
       API.get("matriculas", `idAluno=${idAluno}`).then((res) => {
@@ -16,9 +39,24 @@ export default function ConsultaMatricula() {
 
 
 
-
    return (
       <div className="container-main">
+         <div>
+            <Modal isOpen={modal} toggle={toggle} {...args} backdrop={false} >
+               <ModalHeader toggle={toggle}>Desfazer matricula</ModalHeader>
+               <ModalBody>
+                  
+               </ModalBody>
+               <ModalFooter>
+                  <Button color="secondary" onClick={toggle}>
+                     Cancelar
+                  </Button>
+                  <Button color="danger" onClick={desfazerMatricula}>
+                     Desfazer
+                  </Button>
+               </ModalFooter>
+            </Modal>
+         </div>
          <div className='page-title head-matricula'>
             <div className="titles">
                <h1 className='title_'>Matrícula</h1>
@@ -38,6 +76,7 @@ export default function ConsultaMatricula() {
                      <th>Dia</th>
                      <th>Horário</th>
                      <th>Profesor</th>
+                     <th></th>
                   </tr>
                </thead>
                <tbody>
@@ -52,7 +91,9 @@ export default function ConsultaMatricula() {
                               <td>{matricula.turma.diaSemana}</td>
                               <td>{matricula.turma.horario}</td>
                               <td>{matricula.turma.professor.nomeCompleto}</td>
-                        </tr>
+                              <td><button value={matricula.id} className='btn-undo' onClick={openModal}><span class="material-symbols-rounded">undo
+                              </span>Desfazer</button></td>
+                           </tr>
                         )
                      })
                   }
