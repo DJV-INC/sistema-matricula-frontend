b7998a2a90e269b9f7fc0f12f4e2c158374826d5
douglasdans <douglas.dans@gmail.com>
2023-11-22 21:28:33 -0300

    Dashboard | Corrigida a exibição de alunos matriculados

--- src/components/funcionario/Dashboard/Dashboard.jsx
+++ src/components/funcionario/Dashboard/Dashboard.jsx
@@ -34,72 +34,78 @@ export default function Dashboard() {
       })
    }, []);
 
-  return (
-   <div className='container-main'>
-      <div className='page-title'>
-         <h1 className='title_'>Dashboard</h1>
-         <p className='subtitle_'>Registros</p>
-      </div>
+   console.log();
 
-      <div className="stats-cards-container">
-         <div className="stats-card">
-            <span className='stats-number'>{alunos.length || 0 }</span>
-            <span className='stats-title'>Alunos Cadastrados</span>
-         </div>
-         <div className="stats-card">
-            <span className='stats-number'>{alunos.length || 0 }</span>
-            <span className='stats-title'>Alunos Matriculados</span>
-         </div>
-         <div className="stats-card">
-            <span className='stats-number'>{professores.length || 0}</span>
-            <span className='stats-title'>Professores Cadastrados</span>
-         </div>
-         <div className="stats-card">
-            <span className='stats-number'>{disciplinas.length || 0}</span>
-            <span className='stats-title'>Disciplinas Cadastradas</span>
-         </div>
-         {/* <div className="stats-card">
-            <span className='stats-number'>{qntdTurmas}</span>
-            <span className='stats-title'>Turmas Cadastradas</span>
-         </div>
-         <div className="stats-card">
-            <span className='stats-number'>45</span>
-            <span className='stats-title'>Boletins Cadastrados</span>
-         </div> */}
-      </div>
-
-      <br />
-      <h1>Mais Informações</h1>
-      <br />
-
-      <div className="grafico-container">
-         <div className="stats-card">
-            <div className='grafico-titulo'>
-               <h2>Alunos cadastrados e matriculados</h2>
-               {/* <Select state={chartSelectState1} setState={setChartSelectState1} icon={"tune"}>
-                  <option value="status">Alunos Matriculados</option>
-                  <option value="status">Professores Cadastrados</option>
-               </Select> */}
+   try {
+      return (
+         <div className='container-main'>
+            <div className='page-title'>
+               <h1 className='title_'>Dashboard</h1>
+               <p className='subtitle_'>Registros</p>
             </div>
-            <div className='grafico'>
-               <GraficoLinha data={graficoAlunos}/>
+      
+            <div className="stats-cards-container">
+               <div className="stats-card">
+                  <span className='stats-number'>{alunos.length || 0 }</span>
+                  <span className='stats-title'>Alunos Cadastrados</span>
+               </div>
+               <div className="stats-card">
+                  <span className='stats-number'>{graficoAlunos[0].matriculados || 0 }</span>
+                  <span className='stats-title'>Alunos Matriculados</span>
+               </div>
+               <div className="stats-card">
+                  <span className='stats-number'>{professores.length || 0}</span>
+                  <span className='stats-title'>Professores Cadastrados</span>
+               </div>
+               <div className="stats-card">
+                  <span className='stats-number'>{disciplinas.length || 0}</span>
+                  <span className='stats-title'>Disciplinas Cadastradas</span>
+               </div>
+               {/* <div className="stats-card">
+                  <span className='stats-number'>{qntdTurmas}</span>
+                  <span className='stats-title'>Turmas Cadastradas</span>
+               </div>
+               <div className="stats-card">
+                  <span className='stats-number'>45</span>
+                  <span className='stats-title'>Boletins Cadastrados</span>
+               </div> */}
+            </div>
+      
+            <br />
+            <h1>Mais Informações</h1>
+            <br />
+      
+            <div className="grafico-container">
+               <div className="stats-card">
+                  <div className='grafico-titulo'>
+                     <h2>Alunos cadastrados e matriculados</h2>
+                     {/* <Select state={chartSelectState1} setState={setChartSelectState1} icon={"tune"}>
+                        <option value="status">Alunos Matriculados</option>
+                        <option value="status">Professores Cadastrados</option>
+                     </Select> */}
+                  </div>
+                  <div className='grafico'>
+                     <GraficoLinha data={graficoAlunos}/>
+                  </div>
+               </div>
+      
+               <div className="stats-card">
+                  <div className='grafico-titulo'>
+                     <h2>Quantidade de turmas por disciplina</h2>
+                     {/* <Select state={chartSelectState2} setState={setChartSelectState2} icon={"tune"}>
+                        <option value="status">Alunos por disciplina</option>
+                        <option value="status">Professores por disciplina</option>
+                     </Select> */}
+                  </div>
+                  <div className='grafico'>
+                     <GraficoPizza data={graficoTurmas[0]}/>
+                  </div>
+               </div>
+      
             </div>
          </div>
-
-         <div className="stats-card">
-            <div className='grafico-titulo'>
-               <h2>Quantidade de turmas por disciplina</h2>
-               {/* <Select state={chartSelectState2} setState={setChartSelectState2} icon={"tune"}>
-                  <option value="status">Alunos por disciplina</option>
-                  <option value="status">Professores por disciplina</option>
-               </Select> */}
-            </div>
-            <div className='grafico'>
-               <GraficoPizza data={graficoTurmas[0]}/>
-            </div>
-         </div>
-
-      </div>
-   </div>
-  )
+        )
+   } catch (error) {
+      
+   }
 }
